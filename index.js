52fe535e405dfb09fd178e1b85c6e85f76f3d75c
Terrence Eve <tijayeve@gmail.com>
2019-11-20 00:30:38 -0600

    added first colorBox but other boxes are not appearing. the app is only calling the first red box and not the others

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 // Write your app here! (HINT: First thing should be a call to ReactDOM.render()... )
 
+const HelloWorld = () => {
+    return(
+        <div><h1>Hello World!</h1></div>
+    )
+}
+
+const ColorBox = (props) => {
+
+    let style = {
+        backgroundColor: props.color,
+        // flex: 1,
+        width: "200px",
+        height: "100vh"
+    }
+    console.log(style)
+    return(
+        <div style= {style} >Hello World!</div>
+    )
+}
+
 const RandomizeDiv = () => {
     return( 
-        <div class="text-center bg-dark fixed-top">
-            <button class="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
+        <div className="text-center bg-dark fixed-top">
+            <button className="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
         </div>
     )
 }
 
 const ViewScreen = (props) => {
     return (
-        <div style="background-color: blue" class="w-100 d-flex flex-column align-items-center justify-content-center">
+        <div style="background-color: blue" className="w-100 d-flex flex-column align-items-center justify-content-center">
             <h1>#FF00FF</h1>
-            <button class="btn btn-dark">UNLOCK</button>
+            <button className="btn btn-dark">UNLOCK</button>
         </div>
     )
 }
 
 const App = () => {
-
-    render() {
-        return(
-            <div>
-                <RandomizeDiv /> 
-                {/* <ViewScreen /> */}
-            </div>
+    // render()
+    return(
+        <div>
+            <RandomizeDiv />
+            <ColorBox color="red"/>
+            <ColorBox color="white"/>
+            <ColorBox color="blue"/>
+            {/* <ViewScreen /> */}
+        </div>
         )
     }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
