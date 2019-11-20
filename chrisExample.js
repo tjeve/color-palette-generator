3416cc0a72dd39b5d788d22e15e57d8dcd5b3de5
Terrence Eve <tijayeve@gmail.com>
2019-11-19 18:49:28 -0600

    Added Chris's code in a separate file called chrisExample.js

---
+++ chrisExample.js
@@ -0,0 +1,101 @@
+
+//  Code Revisions 3 Stars 1
+// <script src="https://gist.github.com/oakmac/fe43a379dc9699ceb71debda3a14546d.js"></script>
+  
+// Color Boxes with React.js
+//  app.jsx
+
+function ColorBox (props, idx, _allBoxes) {
+  const style = {
+    border: "2px solid black",git 
+    backgroundColor: props.color,
+    width: "100px",
+    height: "200px"
+  }
+
+  let btnText = 'Unlocked'
+  if (props.isLocked) {
+    btnText = 'Locked'
+  }
+
+  return (
+    <div key={idx} style={style}>
+      <button onClick={props.toggleLock}>{btnText}</button>
+      <h1>{props.color}</h1>
+    </div>
+  )
+}
+
+class App extends React.Component {
+  state = {
+    boxes: [
+      this._newBox(),
+      this._newBox(),
+      this._newBox()
+    ]
+  }
+
+  _randomColor() {
+    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  }
+
+  _newBox() {
+    return {
+      color: this._randomColor(),
+      isLocked: false
+    }
+  }
+
+  randomize() {
+    const currentBoxes = [...this.state.boxes]
+    const newBoxes = currentBoxes.map((box) => {
+      if (!box.isLocked) {
+        box.color = this._randomColor();
+      }
+      return box;
+    })
+    this.setState({boxes: newBoxes})
+  }
+
+  toggleLock(idx) {
+    // TODO: check here that idx is a valid array index
+    const newBoxes = [...this.state.boxes]
+    newBoxes[idx].isLocked = !newBoxes[idx].isLocked
+    this.setState({boxes: newBoxes})
+  }
+
+  addBox() {
+    const newBoxes = [...this.state.boxes, this._newBox()]
+    this.setState({boxes: newBoxes})
+  }
+
+  removeBox() {
+    const newBoxes = [...this.state.boxes]
+    newBoxes.pop()
+    this.setState({boxes: newBoxes})
+  }
+
+  render() {
+    // add the .toggleLock function to each box
+    const boxesWithClickFn = this.state.boxes.map((box, idx) => {
+      box.toggleLock = () => this.toggleLock(idx)
+      return box
+    })
+
+    return (
+      <div className="container">
+          <div className="text-center">
+              <button onClick={() => this.randomize()}>Randomize!</button>
+              <button onClick={() => this.addBox()}>Add Box</button>
+              <button onClick={() => this.removeBox()}>Remove Box</button>
+              <div className="d-flex justify-content-center">
+                {boxesWithClickFn.map(ColorBox)}
+              </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your app here! (HINT: First thing should be a call to ReactDOM.render()... )
+
+const RandomizeDiv = () => {
+    return( 
+        <div class="text-center bg-dark fixed-top">
+            <button class="btn btn-secondary m-1">RANDOMIZE COLORS</button>  
+        </div>
+    )
+}
+
+const ViewScreen = (props) => {
+    return (
+        <div style="background-color: blue" class="w-100 d-flex flex-column align-items-center justify-content-center">
+            <h1>#FF00FF</h1>
+            <button class="btn btn-dark">UNLOCK</button>
+        </div>
+    )
+}
+
+const App = () => {
+
+    render() {
+        return(
+            <div>
+                <RandomizeDiv /> 
+                {/* <ViewScreen /> */}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+/* 
+
+// Lessons Learned/Reminders
+
+*  React JSX expressions can only have one parent element, so wrap everything in the return statement
+in a div.
+
+*/
