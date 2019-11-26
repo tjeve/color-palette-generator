e14c9b448bb5bc8aaf59c1628cda0c2ce4180dbe
Terrence Eve <tijayeve@gmail.com>
2019-11-26 00:20:43 -0600

    placed function return inside of render component of App function

--- .vscode/public/index.js
+++ .vscode/public/index.js
@@ -33,17 +33,33 @@ const RandomizeDiv = () => {
     )
 }
 
-const App = () => {
+class App extends React.Component {
+    //The initial state is set as 3 newBoxes
+    state = {
+        boxes: [this._newBox(), this._newBox(), this._newBox()]
+    }
 
-    // render()
-    return(
-        <div>
-            <RandomizeDiv />
-            <ColorBox color="yellow" width="100"/>
-            <ColorBox color="red" width="100" />
-        </div>
+    _newBox() {
+        return {
+            color: this._randomColor()
+
+        }
+    }
+
+    _randomColor() {
+        return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    }
+
+    render () {
+        return (
+            <div>
+                <RandomizeDiv />
+                <ColorBox color="yellow" width="100"/>
+                {/* <div>{this.state.boxes}</div> */}
+            </div> 
         )
     }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
@@ -55,5 +71,7 @@ ReactDOM.render(<App />, document.getElementById("root"))
 
 *  React JSX expressions can only have one parent element, so wrap everything in the return statement
 in a div.
+* If App is defined as a class, then your return statement must be in nested in a render component. Before when you
+wrote App as a function definition you only needed a return.
 
 */
